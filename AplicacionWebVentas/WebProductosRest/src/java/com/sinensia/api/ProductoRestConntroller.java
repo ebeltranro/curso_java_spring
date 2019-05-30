228633cc9825c1880ce0cef78eb0bff185188a06
Elena Beltran <ebeltranro@gmail.com>
2019-05-30 14:24:23 +0200

    30-05-2019

--- AplicacionWebVentas/WebProductosRest/src/java/com/sinensia/api/ProductoRestConntroller.java
+++ AplicacionWebVentas/WebProductosRest/src/java/com/sinensia/api/ProductoRestConntroller.java
@@ -9,6 +9,9 @@ import com.google.gson.Gson;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -21,12 +24,12 @@ import javax.servlet.http.HttpServletResponse;
  */
 //en lugar de crear un servlet, hemos creado una clase extendiendo a HttpServlet
 //vamos a usar unas anotaciones para indicar que esto es un servlet (decoradores)
-@WebServlet(asyncSupported = true, urlPatterns = "/api.productos") //indicamos que admite comunicación asíncrona 
+@WebServlet(asyncSupported = true, urlPatterns = "/api/productos") //indicamos que admite comunicación asíncrona 
 //vamos a hacer algunas coass un poco diferentes para practicar
 public class ProductoRestConntroller extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    protected void doPut(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         PrintWriter escritorRespuesta = response.getWriter();
         response.setContentType("application/json;charset=UTF-8");
@@ -44,16 +47,83 @@ public class ProductoRestConntroller extends HttpServlet {
 
         //pasamos de JSON a java para modificar (a clase producto)
         Gson gson = new Gson();
-        
+
         Producto producto = gson.fromJson(textoJson.toString(), Producto.class);
-        
-        producto.setNombre(producto.getNombre());
-        producto.setPrecio("500 bolivares");
+
+        //llamamos a un servicio que se encarga de hacer la transformación
+        ServicioProductoSingleton sps = ServicioProductoSingleton.getInstancia(); //el unico objeto de esta clase
+        sps.modificar(producto);
+
+        /*producto.setNombre(producto.getNombre());
+        producto.setPrecio("500 bolivares");*/
         //transformamos a formato JSON para enviar la respuesta
         String jsonRespuesta = gson.toJson(producto);
-        
+
         escritorRespuesta.println(jsonRespuesta);
 
     }
 
+    //vamos a hacer un método POST para insertar producto
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter escritorRespuesta = response.getWriter();
+        response.setContentType("application/json;charset=UTF-8");
+
+        BufferedReader bufRead = request.getReader();
+
+        StringBuilder textoJson = new StringBuilder();
+
+        for (String lineaJson = bufRead.readLine(); lineaJson != null; lineaJson = bufRead.readLine()) {
+            textoJson.append(lineaJson);
+        }
+
+        bufRead.close();
+        Producto producto = null;
+
+        Gson gson = new Gson();
+        producto = gson.fromJson(textoJson.toString(), Producto.class);
+        String preciodevuelto = producto.getPrecio();
+
+        ServicioProductoSingleton sps = ServicioProductoSingleton.getInstancia();
+
+        String mensaje = null;
+
+        if (producto.getNombre() != null && producto.getPrecio() != null) {
+            sps.insertar(producto);
+            mensaje = "se ha insertado correctamente";
+        } else {
+            mensaje = "No se ha insertado correctamente";
+        }
+
+        String jsonRespuesta = gson.toJson(mensaje);
+        escritorRespuesta.println(jsonRespuesta);
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter escritorRespuesta = response.getWriter();
+        response.setContentType("application/json;charset=UTF-8");
+        
+        ArrayList<Producto> listaLeida = new ArrayList<Producto>();
+        ServicioProductoSingleton sps = ServicioProductoSingleton.getInstancia();
+        listaLeida = sps.obtenerTodos();
+        
+        //Producto productoLeido = listaLeida.get(0);
+        //Producto productoLeido1 = listaLeida.get(1);
+        
+        
+        Gson gson = new Gson();
+        //System.out.println("brea");
+        //System.out.println(productoLeido.getNombre() + productoLeido1.getNombre());
+        
+        
+        String jsonRespuesta = gson.toJson(listaLeida);
+        escritorRespuesta.println(jsonRespuesta);
+        //String jsonRespuesta = gson.toJson(sps.obtenerTodos());
+        //escritorRespuesta.println(jsonRespuesta);
+        
+    }
 }
---
+++ AplicacionWebVentas/WebProductosRest/src/java/com/sinensia/api/ServicioProductoSingleton.java
@@ -0,0 +1,48 @@
+package com.sinensia.api;
+
+import java.util.ArrayList;
+
+/*
+ * Clase patrón Singleton, solo puede haber una instancia (objeto)
+en toda la aplicación de este tipo de clase
+ */
+//la única instancia es privada
+public class ServicioProductoSingleton {
+    //una única variable estática y privada
+    public ArrayList<Producto> listaProductos;
+    
+    public void insertar (Producto p){
+        listaProductos.add(p);
+    }
+    
+    public Producto modificar (Producto p){
+        p.setNombre(p.getNombre() + " - Modificado");
+        p.setPrecio(p.getPrecio() + " - Modificado");
+        return p;
+    }
+    
+    public ArrayList obtenerTodos() {
+        //return (Producto[]) listaProductos.toArray(); //un cast para que los productos con forma objeto que salen del Array pasen a forma producto
+        return listaProductos;
+    }
+    
+    private static ServicioProductoSingleton instancia = null;
+
+    //los singleton tienen constructores privados; nadie puede hacer new dentro de esta clase
+    //puede ser protected
+    private ServicioProductoSingleton() {
+        //igual que poner super() --> constructor de Object
+        this.listaProductos = new ArrayList<>();
+    }
+    //La primera vez que se llama al método, se crea la instancia
+    //A partir de ese momento hasta que la aplicación termine,
+    //la instancia seguirá viva y es devuelta por el método
+    //venga de donde venga la llamada.
+    //La instancia nunca es nula
+    public static ServicioProductoSingleton getInstancia() {
+        if (instancia == null)
+            instancia = new ServicioProductoSingleton(); //igual al único objeto de la clase SercicioProductoSingleton. 
+        return instancia;
+    }
+
+}
